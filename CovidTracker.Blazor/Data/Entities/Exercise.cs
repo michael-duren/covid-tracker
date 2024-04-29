00bae4c8cebee4a1d1a4d4dc1457e645f2f28502
Michael Duren <michaeld@michaelduren.com>
2024-04-28 21:19:07 -0500

    add icons in footer

---
+++ CovidTracker.Blazor/Data/Entities/Exercise.cs
@@ -0,0 +1,7 @@
+namespace CovidTracker.Blazor.Data.Entities;
+
+public class Exercise
+{
+    public int Id { get; set; }
+    public int ExerciseTypeId { get; set; }
+}
---
+++ CovidTracker.Blazor/Data/Entities/ExerciseType.cs
@@ -0,0 +1,7 @@
+namespace CovidTracker.Blazor.Data.Entities;
+
+public class ExerciseType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
